6c77fe3a0e870e9182b55c1dc85460df906b4ad6
Ilya Motkin <imotkin@outlook.com>
2026-04-07 21:25:47 +0300

    Update L1.11

--- L1/11/main.go
+++ L1/11/main.go
@@ -11,12 +11,13 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
 	"slices"
 )
 
-func unique(nums ...int) map[int]struct{} {
-	m := make(map[int]struct{})
+func unique[T cmp.Ordered](nums ...T) map[T]struct{} {
+	m := make(map[T]struct{})
 
 	for _, n := range nums {
 		m[n] = struct{}{}
@@ -25,22 +26,37 @@ func unique(nums ...int) map[int]struct{} {
 	return m
 }
 
-func main() {
-	a := []int{1, 2, 3}
-	b := []int{2, 3, 4, 5}
+func intersection[T cmp.Ordered](a, b []T) []T {
+	var (
+		uniqueA = unique(a...)
+		uniqueB = unique(b...)
+		common  []T
+	)
 
-	uniqueA := unique(a...)
-	uniqueB := unique(b...)
-
-	var common []int
-
-	for num := range uniqueA {
-		if _, ok := uniqueB[num]; ok {
-			common = append(common, num)
+	for v := range uniqueA {
+		if _, ok := uniqueB[v]; ok {
+			common = append(common, v)
 		}
 	}
 
 	slices.Sort(common)
 
-	fmt.Println(common)
+	return common
+}
+
+func Print[T cmp.Ordered](a, b []T) {
+	fmt.Printf("%v ∩ %v = %v\n", a, b, intersection[T](a, b))
+
+}
+
+func main() {
+	intA := []int{1, 2, 3}
+	intB := []int{2, 3, 4, 5}
+
+	Print(intA, intB)
+
+	strA := []string{"a", "b", "c", "d", "e"}
+	strB := []string{"a", "c", "e"}
+
+	Print(strA, strB)
 }
