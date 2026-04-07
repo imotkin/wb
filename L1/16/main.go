2e896b699fe8cd3cc63d04250ed5bdb726fc1e9a
Ilya Motkin <imotkin@outlook.com>
2026-04-07 23:26:54 +0300

    L1.16

---
+++ L1/16/main.go
@@ -0,0 +1,43 @@
+// Задача L1.16 - Быстрая сортировка
+//
+// Реализовать алгоритм быстрой сортировки массива встроенными средствами языка.
+// Можно использовать рекурсию.
+//
+// Подсказка: напишите функцию quickSort([]int) []int которая сортирует срез
+// целых чисел. Для выбора опорного элемента можно взять середину или первый элемент.
+
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func quickSort(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+
+	var less, equal, more []int
+
+	pivot := nums[0]
+
+	for _, v := range nums {
+		switch {
+		case v < pivot:
+			less = append(less, v)
+		case v > pivot:
+			more = append(more, v)
+		default:
+			equal = append(equal, v)
+		}
+	}
+
+	return slices.Concat(quickSort(less), quickSort(equal), quickSort(more))
+}
+
+func main() {
+	nums := []int{10, 2, 3, 7, 5, 1, 20, 0}
+
+	fmt.Println(quickSort(nums))
+}
